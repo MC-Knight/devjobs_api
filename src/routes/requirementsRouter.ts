f1431ffae41310ccc14a6da4c014bdeac8cc951e
Mc-Knight <tetobobo1@gmail.com>
2024-04-19 12:13:43 +0200

    ft: authorization-checkup

--- src/routes/requirementsRouter.ts
+++ src/routes/requirementsRouter.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import RequirementController from "../controllers/jobRequirementsController";
+import authMiddleware from "../middleware/authMiddleware";
+import isAdminMiddleware from "../middleware/verifyAccess";
 
 const routerJobRequirements = express();
 
-routerJobRequirements.post("", RequirementController.createRequirement);
-routerJobRequirements.put("/:id", RequirementController.updateRequirement);
+routerJobRequirements.post(
+  "",
+  authMiddleware,
+  isAdminMiddleware,
+  RequirementController.createRequirement
+);
+routerJobRequirements.put(
+  "/:id",
+  authMiddleware,
+  isAdminMiddleware,
+  RequirementController.updateRequirement
+);
 
 export default routerJobRequirements;
--- src/routes/roleRouter.ts
+++ src/routes/roleRouter.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import RoleController from "../controllers/jobRoleController";
+import authMiddleware from "../middleware/authMiddleware";
+import isAdminMiddleware from "../middleware/verifyAccess";
 
 const routerJobRole = express();
 
-routerJobRole.post("", RoleController.createRole);
-routerJobRole.put("/:id", RoleController.updateRole);
+routerJobRole.post(
+  "",
+  authMiddleware,
+  isAdminMiddleware,
+  RoleController.createRole
+);
+routerJobRole.put(
+  "/:id",
+  authMiddleware,
+  isAdminMiddleware,
+  RoleController.updateRole
+);
 
 export default routerJobRole;
